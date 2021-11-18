f95dce19542174463eb53b692f1b8507d68409b4
ubuntu <anish.gottapu@keysight.com>
2021-11-18 15:53:09 +0530

    add do.py go lint

--- do.py
+++ do.py
@@ -126,7 +126,7 @@ def lint():
 def go_lint():
     run(["curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.43.0"])
     os.chdir("gosnappi")
-    run(["golangci-lint run -v"])
+    run(["golangci-lint run -v --modules-download-mode mod"])
 
 
 def test():
